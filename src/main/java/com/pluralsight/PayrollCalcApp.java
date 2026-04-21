7fee95edc0d0f18199761eb4df840dc1ba6e33e8
Gabe <Carrollgbusiness@gmail.com>
2026-04-21 11:16:04 -0500

    First commit

--- src/main/java/com/pluralsight/PayrollCalcApp.java
+++ src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class PayrollCalcApp {
+    public static void main(String[] args) {
+
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader theFile = new BufferedReader(fileReader);
+            String line;
+
+
+            while ((line = theFile.readLine()) != null){
+                String[] splitPipe = line.split("\\|");
+
+                System.out.println(splitPipe[0]);
+            }
+
+            Employee.
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //System.out.println(line);
+        //System.out.println(Arrays.toString(splitPipe));
+
+
+
+    }
 }
