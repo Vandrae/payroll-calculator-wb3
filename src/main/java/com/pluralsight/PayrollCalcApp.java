a6548ba040a0605e5ff84d9e0c8fc17f7718ddf9
Gabe <Carrollgbusiness@gmail.com>
2026-04-21 14:32:21 -0500

    Added comments

--- src/main/java/com/pluralsight/PayrollCalcApp.java
+++ src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -8,23 +8,29 @@ public class PayrollCalcApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        //asks what file they want to read with an input for an answer
         System.out.print("What file do you want to read? ");
         String read = input.nextLine();
 
+        //asks what file they want to write with the ability to create it
         System.out.print("What file do you want to write ? ");
         String write = input.nextLine();
 
 
         try {
+            //reader and writer both need a buffer calls the input from the users to locate/create files
             FileReader fileReader = new FileReader("src/main/resources/" + read);
             FileWriter writer = new FileWriter("src/main/resources/" + write);
             BufferedReader theFile = new BufferedReader(fileReader);
             BufferedWriter bwriter = new BufferedWriter(writer);
 
-            theFile.readLine();
+            //holds first line
             String line;
+            //reads first line (i think)
+            theFile.readLine();
 
             bwriter.write("id|name|gross pay \n");
+
             while ((line = theFile.readLine()) != null){
                 System.out.println(line);
                 String[] splitPipe = line.split("\\|");
@@ -34,13 +40,15 @@ public class PayrollCalcApp {
                 System.out.println("Pay Rate: " + splitPipe[3]);
                 System.out.println(" ");
 
+                //employee object that prints values parsed on lines 35-41 formatted in "id|name|gross pay"
                 Employee employee = new Employee(Integer.parseInt(splitPipe[0]),splitPipe[1], Double.parseDouble(splitPipe[2]),Double.parseDouble(splitPipe[3]));
                 bwriter.write(employee.getEmployeeID() + "|" + employee.getName() + "|" + employee.getGrossPay() + "\n");
 
             }
+
+           //after loop terminates closes writer and bwriter
             bwriter.close();
             writer.close();
-
 
         } catch (Exception e) {
             throw new RuntimeException(e);
