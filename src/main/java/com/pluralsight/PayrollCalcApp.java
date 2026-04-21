8cc8891e6c9ccf66ba93f1a12fb63b1c0f0a1495
Gabe <Carrollgbusiness@gmail.com>
2026-04-21 14:22:40 -0500

    added employee object, filewriter/bufferedwriter, ability for user to tell what file to read and write to

--- src/main/java/com/pluralsight/PayrollCalcApp.java
+++ src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -1,34 +1,49 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class PayrollCalcApp {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What file do you want to read? ");
+        String read = input.nextLine();
+
+        System.out.print("What file do you want to write ? ");
+        String write = input.nextLine();
+
 
         try {
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            FileReader fileReader = new FileReader("src/main/resources/" + read);
+            FileWriter writer = new FileWriter("src/main/resources/" + write);
             BufferedReader theFile = new BufferedReader(fileReader);
+            BufferedWriter bwriter = new BufferedWriter(writer);
+
+            theFile.readLine();
             String line;
 
-
+            bwriter.write("id|name|gross pay \n");
             while ((line = theFile.readLine()) != null){
+                System.out.println(line);
                 String[] splitPipe = line.split("\\|");
+                System.out.println("ID: " + splitPipe[0]);
+                System.out.println("Name: " + splitPipe[1]);
+                System.out.println("hours worked: " + splitPipe[2]);
+                System.out.println("Pay Rate: " + splitPipe[3]);
+                System.out.println(" ");
 
-                System.out.println(splitPipe[0]);
+                Employee employee = new Employee(Integer.parseInt(splitPipe[0]),splitPipe[1], Double.parseDouble(splitPipe[2]),Double.parseDouble(splitPipe[3]));
+                bwriter.write(employee.getEmployeeID() + "|" + employee.getName() + "|" + employee.getGrossPay() + "\n");
+
             }
+            bwriter.close();
+            writer.close();
 
-            Employee.
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        //System.out.println(line);
-        //System.out.println(Arrays.toString(splitPipe));
-
-
-
     }
 }
